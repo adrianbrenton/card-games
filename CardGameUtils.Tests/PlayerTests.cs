abb5d742f95f059010c842dd5a14d5a8e249c2e7
adrianbrenton <adrian.r.brenton@gmail.com>
2018-05-07 23:01:05 +0100

    Add unit test for Player class

---
+++ CardGameUtils.Tests/PlayerTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xunit;
+using Xunit.Abstractions;
+namespace CardGameUtils.Tests
+{
+    public class PlayerTests
+    {
+        public PlayerTests()
+        {
+
+        }
+
+        [Fact]
+        public void PlayCard_CardNotInHand_ShouldThrowException()
+        {
+            Player player = new Player();
+            Card card = new Card(Rank.Ace, Suit.Spades);
+            ArgumentOutOfRangeException exception = Assert.Throws<ArgumentOutOfRangeException>(() => player.PlayCard(card));
+            Assert.StartsWith("Chosen card not in hand", exception.Message);
+
+
+        }
+    }
+}
