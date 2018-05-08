7b806f56f97aa4a566b43319558b189f07f33c42
adrianbrenton <adrian.r.brenton@gmail.com>
2018-05-08 23:18:17 +0100

    Add PlayCard_CardInHand_ShouldRemoveCard()

--- CardGameUtils.Tests/PlayerTests.cs
+++ CardGameUtils.Tests/PlayerTests.cs
@@ -5,20 +5,30 @@ namespace CardGameUtils.Tests
 {
     public class PlayerTests
     {
+        Card card;
+        Player player;
+
         public PlayerTests()
         {
-
+            player = new Player();
+            card = new Card(Rank.Ace, Suit.Spades);
         }
 
         [Fact]
         public void PlayCard_CardNotInHand_ShouldThrowException()
         {
-            Player player = new Player();
-            Card card = new Card(Rank.Ace, Suit.Spades);
             ArgumentOutOfRangeException exception = Assert.Throws<ArgumentOutOfRangeException>(() => player.PlayCard(card));
             Assert.StartsWith("Chosen card not in hand", exception.Message);
 
 
+
+        }
+
+        [Fact]
+        public void PlayCard_CardInHand_ShouldRemoveCard(){
+            player.ReceiveCard(card);
+            Assert.Equal(card, player.PlayCard(card));
+            Assert.Throws<ArgumentOutOfRangeException>(() => player.PlayCard(card));
         }
     }
 }
