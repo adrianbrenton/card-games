28089338e5359226a0bf20a48fe20cfad32b10e0
adrianbrenton <adrian.r.brenton@gmail.com>
2018-05-09 01:22:08 +0100

    Add PlayRound() method

--- Blackjack/BlackjackGame.cs
+++ Blackjack/BlackjackGame.cs
@@ -4,6 +4,9 @@ namespace Blackjack
 {
     public class BlackjackGame
     {
-        
+        public void PlayRound()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
