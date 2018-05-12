5a26cb6f690b203ff89bf94934998cd55690cd37
adrianbrenton <adrian.r.brenton@gmail.com>
2018-05-12 16:45:31 +0100

    Remove excess whitespace

--- CardGames/Program.cs
+++ CardGames/Program.cs
@@ -15,7 +15,6 @@ namespace CardGames
         public static void Main(string[] args)
         {
             BuildWebHost(args).Run();
-
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
