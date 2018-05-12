99b54baa4d5f5fa679beee96cae493a3d108ea37
adrianbrenton <adrian.r.brenton@gmail.com>
2018-05-12 16:43:11 +0100

    Add reference to CardGameUtils.csproj

---
+++ Blackjack/BlackjackPlayer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Blackjack
+{
+    public class BlackjackPlayer
+    {
+        public BlackjackPlayer()
+        {
+        }
+    }
+}
---
+++ Blackjack/PlayerStatus.cs
@@ -0,0 +1,7 @@
+﻿using System;
+namespace Blackjack
+{
+    public enum PlayerStatus
+    {
+    }
+}
