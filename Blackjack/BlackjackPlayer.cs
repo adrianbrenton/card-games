7480745828b99608f9338e4e314dcbd5f94c08f7
adrianbrenton <adrian.r.brenton@gmail.com>
2018-05-13 23:21:46 +0100

    Override ReceiveCard method with helper functions
    When receiving a card, a BlackjackPlayer must decide if its hand is
    above, below, or equal to the targetValue (=21 by default). Hence, we
    must override the ReceiveCard() method inherited from Player.
    
    BestPossibleHand() could have been implemented much more simply if
    the targetValue was always 21 as it is in Casinos, but I intend to add
    in the option to change the targetValue and number of decks later on,
    so this method had to be designed to work for any number of decks and
    for any unsigned integer targetValue.

--- Blackjack/BlackjackPlayer.cs
+++ Blackjack/BlackjackPlayer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 using CardGameUtils;
 
@@ -6,9 +7,50 @@ namespace Blackjack
 {
     public class BlackjackPlayer: Player
     {
-        
+        PlayerStatus status;
+        public PlayerStatus Status { get { return status; } }
+
+        uint minHandValue;
+        uint handScore; //highest possible hand-value without exceeding targetValue
+        uint targetValue; //needed if target-value is variable
+        uint numberOfaces; // needed if target-value is variable
+
         public BlackjackPlayer()
         {
+            targetValue = 21;
+        }
+
+        public override void ReceiveCard(Card card)
+        {
+            base.ReceiveCard(card);
+            if (card.rank == Rank.Ace) { numberOfaces++; }
+            minHandValue += EvaluateCard(card);
+            if (minHandValue > targetValue) { status = PlayerStatus.Bust; }
+            else { handScore = BestPossibleHandScore(); }
+            if (handScore == targetValue) { status = PlayerStatus.OnTarget; }
+        }
+
+        uint EvaluateCard(Card card)
+        {
+            if ((uint)card.rank > 10)
+            {
+                return 10;
+            }
+            return (uint)card.rank;
+        }
+
+        /* This can be done much more simply if targetValue is always 21 like in
+         * the usual casino version of Blackjack. However, this is not always
+         * the case for this version of the game */
+        uint BestPossibleHandScore()
+        {
+            uint score = minHandValue;
+            for (int i = 1; i <= numberOfaces; i++)
+            {
+                if (score + 10 <= targetValue) { score += 10; }
+                else { return score; }
+            }
+            return score;
         }
     }
 }
