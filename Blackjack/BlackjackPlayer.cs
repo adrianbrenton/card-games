3b03dc6b0a0af4f44aed0f52c4fd7f47c1b214b7
adrianbrenton <adrian.r.brenton@gmail.com>
2018-05-13 23:41:25 +0100

    Make BlackjackPlayer.targetValue readonly

--- Blackjack/BlackjackPlayer.cs
+++ Blackjack/BlackjackPlayer.cs
@@ -12,8 +12,8 @@ namespace Blackjack
 
         uint minHandValue;
         uint handScore; //highest possible hand-value without exceeding targetValue
-        uint targetValue; //needed if target-value is variable
-        uint numberOfaces; // needed if target-value is variable
+        readonly uint targetValue;
+        uint numberOfAces; // needed if target-value is variable
 
         public BlackjackPlayer()
         {
@@ -23,7 +23,7 @@ namespace Blackjack
         public override void ReceiveCard(Card card)
         {
             base.ReceiveCard(card);
-            if (card.rank == Rank.Ace) { numberOfaces++; }
+            if (card.rank == Rank.Ace) { numberOfAces++; }
             minHandValue += EvaluateCard(card);
             if (minHandValue > targetValue) { status = PlayerStatus.Bust; }
             else { handScore = BestPossibleHandScore(); }
@@ -45,7 +45,7 @@ namespace Blackjack
         uint BestPossibleHandScore()
         {
             uint score = minHandValue;
-            for (int i = 1; i <= numberOfaces; i++)
+            for (int i = 1; i <= numberOfAces; i++)
             {
                 if (score + 10 <= targetValue) { score += 10; }
                 else { return score; }
