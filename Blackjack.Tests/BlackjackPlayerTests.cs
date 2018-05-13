f428f2c0174d1abd5d713349139fd6e48fb28463
adrianbrenton <adrian.r.brenton@gmail.com>
2018-05-13 23:00:38 +0100

    Add tests for BlackjackPlayer.ReceiveCard()

--- Blackjack.Tests/BlackjackPlayerTests.cs
+++ Blackjack.Tests/BlackjackPlayerTests.cs
@@ -1,14 +1,38 @@
 using System;
 using Xunit;
 
+using CardGameUtils;
+
 namespace Blackjack.Tests
 {
     public class BlackjackPlayerTests
     {
         [Fact]
-        public void  ()
+        public void ReceiveCard_BlackjackHand_ShouldCauseStatusOnTarget()
         {
+            BlackjackPlayer player = new BlackjackPlayer();
+            player.ReceiveCard(new Card(Rank.King, Suit.Hearts));
+            player.ReceiveCard(new Card(Rank.Ace, Suit.Spades));
+            Assert.Equal(PlayerStatus.OnTarget, player.Status);
+        }
 
+        [Fact]
+        public void ReceiveCard_SoftSeventeen_ShouldCauseStatusActive()
+        {
+            BlackjackPlayer player = new BlackjackPlayer();
+            player.ReceiveCard(new Card(Rank.Ace, Suit.Hearts));
+            player.ReceiveCard(new Card(Rank.Six, Suit.Diamonds));
+            Assert.Equal(PlayerStatus.Active, player.Status);
+        }
+
+        [Fact]
+        public void ReceiveCard_ThreeKings_ShouldCauseStatusBust()
+        {
+            BlackjackPlayer player = new BlackjackPlayer();
+            player.ReceiveCard(new Card(Rank.King, Suit.Hearts));
+            player.ReceiveCard(new Card(Rank.King, Suit.Diamonds));
+            player.ReceiveCard(new Card(Rank.King, Suit.Spades));
+            Assert.Equal(PlayerStatus.Bust, player.Status);
         }
     }
 }
