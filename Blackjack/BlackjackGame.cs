c2f517ab803d359690c7ab7fdc77a2a229cfd2ac
adrianbrenton <adrian.r.brenton@gmail.com>
2018-05-14 00:15:45 +0100

    Add constructor and initialize fields

--- Blackjack/BlackjackGame.cs
+++ Blackjack/BlackjackGame.cs
@@ -1,20 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Blackjack
 {
     public class BlackjackGame
     {
-        
+        int numberOfPlayers;
+        BlackjackPlayer player1;
+        BlackjackPlayer player2;
+        BlackjackPlayer player3;
+
+        Queue<BlackjackPlayer> players;
+
         public BlackjackGame()
         {
-            
+            numberOfPlayers = 3;
+            players = new Queue<BlackjackPlayer>(numberOfPlayers);
+            player1 = new BlackjackPlayer();
+            player2 = new BlackjackPlayer();
+            player3 = new BlackjackPlayer();
+            players.Enqueue(player1);
+            players.Enqueue(player2);
+            players.Enqueue(player3);
+
         }
 
     
         public void PlayRound()
         {
-            throw new NotImplementedException();
+            
 
+        }
+
+        private  void DealHands()
+        {
+            
         }
     }
 }
@@ -22,7 +42,12 @@ namespace Blackjack
 /* PLAN:
  * Declare and Instantiate players
  * Create queue of players
- * while not allPlayersFinished
+ * while (not (all players Stuck OR all players OnTarget) )
  * {
- *      for
+ *      currentPlayer = pop a player off the queue
+ *      if currentPlayer.Status = PlayerStatus.Active
+ *      {
+ *          currentPlayer(ReceiveCard(deck.DealNext())
+ *      }
+ *      
  */
