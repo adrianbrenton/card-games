29c6e52c485bd3a48fa907ecc1ddf8f11ca61117
adrianbrenton <adrian.r.brenton@gmail.com>
2018-05-13 11:25:50 +0100

    Begin BlackjackPlayer xunit test class

--- Blackjack.Tests/BlackjackPlayerTests.cs
+++ Blackjack.Tests/BlackjackPlayerTests.cs
@@ -3,10 +3,10 @@ using Xunit;
 
 namespace Blackjack.Tests
 {
-    public class UnitTest1
+    public class BlackjackPlayerTests
     {
         [Fact]
-        public void Test1()
+        public void  ()
         {
 
         }
