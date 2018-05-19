1db218c3d775508bfe42ec66babea2c03eb8bf65
adrianbrenton <adrian.r.brenton@gmail.com>
2018-05-19 16:56:36 +0100

    Add test to confirm ToString() works correctly

--- CardGameUtils.Tests/CardTests.cs
+++ CardGameUtils.Tests/CardTests.cs
@@ -1,10 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
+using Xunit;
+using Xunit.Abstractions;
+
 namespace CardGameUtils.Tests
 {
     public class CardTests
     {
-        public CardTests()
+        [Fact]
+        public void ToString_KingOfHearts_ShouldGiveAsString()
         {
+            Card card = new Card(Rank.King, Suit.Hearts);
+            string expectedString = "King of Hearts";
+            Assert.Equal(expectedString, card.ToString());
         }
     }
 }
